90f98eb370c294a23207568c43e2bd5981d5d7f4
Poul <magik2art@gmail.com>
2021-04-17 13:55:27 +0300

    python 3.8

--- parcent.py
+++ parcent.py
@@ -1,16 +1,13 @@
-# -*- coding: utf-8 -*-
-# К сожалению не смог подключить UTF-8 и кириллицу не отоброжает, поэтому просклонять не удалось - в папке лежит скриншот
 user_text = int(input("Введите число от 0 до 20 "))
-print user_text
+print(user_text)
 text = ()
-# for test1 in range(21):
 i = 0
 for i in range(21):
     if user_text == 1:
-        text = (user_text, " Procent") # Процент
+        text = (user_text, " Процент")
     elif user_text == 2 or user_text == 3 or user_text == 4:
-        text = (user_text, " Procent'a") # Процента
-    elif user_text in range(0,20):
-       text = (user_text, " Procent'ov") # Процентов
-    print text
+        text = (user_text, " Процента")
+    elif user_text in range(0,21):
+       text = (user_text, " Процентов")
+    print(text)
     user_text += 1
